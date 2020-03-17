54595f93f22e2673f2d2c806fbd850a4510c5cf1
Amirmohammad_Bashiri <amirmohamad.bashiri@gmail.com>
2020-03-17 15:18:27 +0330

    Fixed the modal

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import Search from './components/layout/Search';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/logs/AddLogModal';
 
 const App = () => {
 	useEffect(() => {
@@ -14,6 +16,8 @@ const App = () => {
 		<Fragment>
 			<Search />
 			<div className="container">
+				<AddBtn />
+				<AddLogModal />
 				<Logs />
 			</div>
 		</Fragment>
